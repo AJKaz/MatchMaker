9f4289373a5fe2fb45d5509c9f2f7756ce3e656b
Dominic A. <dominicapgar2100@gmail.com>
2024-11-16 13:55:29 -0500

    Edited Sprite Pivots + Menu Icons

--- MatchMaker/Assets/Scripts/MainMenuManager.cs
+++ MatchMaker/Assets/Scripts/MainMenuManager.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public List<Sprite> things = new List<Sprite>();
+    public List<Sprite> icons = new List<Sprite>();
+
+    public Image icon1;
+    public Image icon2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Get a random index
+        int randomIndex = Random.Range(0, icons.Count);
+        icon1.sprite = icons[randomIndex];
+
+        // Get a random index
+        randomIndex = Random.Range(0, icons.Count);
+        icon2.sprite = icons[randomIndex];
+
     }
 
     // Update is called once per frame
